4bc924461e6e7ba97c3195d76af129fb8652535c
Christy Zheng <czheng@Christys-MBP.home>
2020-08-08 15:23:56 -0400

    put firebase in

---
+++ backend/server/config/firebase.js
@@ -0,0 +1,15 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAVNqiE67PiZh2zXf9pLuz9EaZcxAdS6sk",
+    authDomain: "home-forum123.firebaseapp.com",
+    databaseURL: "https://home-forum123.firebaseio.com",
+    projectId: "home-forum123",
+    storageBucket: "home-forum123.appspot.com",
+    messagingSenderId: "8101885066",
+    appId: "1:8101885066:web:ceaa66a6caf1b58bd32973",
+    measurementId: "G-9H59WYWNM1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
