b946c1ecdc7f1601d1fe1524ceaff904b1029c8f
hanguyen0 <nguyha16@gmail.com>
2020-09-24 14:08:24 -0700

    add comments in chatjs

--- backend/server/routes/chat.js
+++ backend/server/routes/chat.js
@@ -1,11 +1,29 @@
 const express = require('express');
-const firebase = require('../config/config.js');
+const {firebase} = require('../config/config.js');
 const router = express.Router()
 require('dotenv').config();
 
 const db = firebase.firestore();
 
+//get messages\
+//this route needs authorization
+router.get("/", (req, res) => {
+    db.collection('messages').add({
+        name: "Christy", //getUserName(),
+        text: message
+        //profilePicUrl: getProfilePicUrl(),
+        //timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+        console.error('Error writing new message to database', error);
+    });
+})
+
 // Saves a new message to your Cloud Firestore database.
+//this route needs authorization
 router.post("/", (req, res) => {
     let message = req.body;
     db.collection('messages').add({
